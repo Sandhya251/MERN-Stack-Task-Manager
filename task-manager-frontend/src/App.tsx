0f2cde07d9bc5cefef7c575a45f9f2638da59ab7
Sandhya Sisodiya <sandhyasisodiya1033@gmail.com>
2025-06-08 09:29:08 +0530

    Add Tailwind and PostCSS config

--- task-manager-frontend/src/App.tsx
+++ task-manager-frontend/src/App.tsx
@@ -6,16 +6,18 @@ import AddTask from './pages/AddTask';
 import EditTask from './pages/EditTask';
 import { AuthProvider } from './auth/AuthContext';
 import PrivateRoute from './auth/PrivateRoute';
-import './index.css';
+
 
 
 
 function App() {
   return (
+     
     <AuthProvider>
       <Router>
         <Routes>
-        
+
+
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
@@ -23,6 +25,9 @@ function App() {
           <Route path="/edit-task/:id" element={<PrivateRoute><EditTask /></PrivateRoute>} />
         </Routes>
       </Router>
+             <div className="bg-red-500 text-white p-4 text-2xl font-bold">
+      Tailwind should color this red with white text.
+    </div>
     </AuthProvider>
   );
 }
